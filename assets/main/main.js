49cd2d5808777a078572eec300126a6ea45e0b82
ehsan ghasemi <ehsan.ghasemi.se7en@gmail.com>
2019-09-13 17:11:47 +0430

    app user email,mobile and status done

--- assets/main/main.js
+++ assets/main/main.js
@@ -301,7 +301,7 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                             '../assets/pages/scripts/ui-toastr.js',
 
 
-                            '../assets/main/controll/resortController.js?1500'
+                            '../assets/main/controll/resortController.js?1501'
                         ]
                     });
                 }]
@@ -865,7 +865,7 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
                             '../assets/pages/scripts/ui-buttons.js',
 
 
-                            '../assets/main/controll/appUsersController.js'
+                            '../assets/main/controll/appUsersController.js?1502'
                         ]
                     });
                 }]
